e5f52511264f0f20cecc0589275168d41a156e61
RomanKost82 <106436537+RomanKost82@users.noreply.github.com>
2024-03-25 14:14:08 +0300

    Added second function to capitalize first letter of each word in string

--- str_func.py
+++ str_func.py
@@ -1,3 +1,7 @@
 def uppercase_string(input_string):
     """ принимает на вход строку и возвращает ее в верхнем регистре"""
     return input_string.upper()
+
+def capitalize_words(input_string):
+    """ делает заглавными первые буквы каждого слова в строке """
+    return input_string.title()
